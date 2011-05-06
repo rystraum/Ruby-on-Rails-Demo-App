af6002a4625964bfb9305a5683a241a791643947
Rystraum Gamonez <rystraum@rystraum.com>
2011-05-06 23:03:53 +0800

    created user has_many microposts and microposts belongs_to user association

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+# Schema version: 20110506143358
+#
+# Table name: microposts
+#
+#  id         :integer         not null, primary key
+#  content    :string(255)
+#  user_id    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Micropost < ActiveRecord::Base
+  belongs_to :user
+  
+  validates :content, :length => { :maximum => 140 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+# Schema version: 20110506143358
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class User < ActiveRecord::Base
+  has_many :microposts
+  
+  validates :name, :presence => true
 end
